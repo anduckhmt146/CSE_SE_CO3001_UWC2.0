b22bc105f138d86b5ddc4bd36fb3c53f2ff76093
Nguyen Duc An <an.nguyenduc1406@hcmut.edu.vn>
2023-02-02 00:20:00 +0700

    update github

--- src/mock_data/account.js
+++ src/mock_data/account.js
@@ -1,8 +1,8 @@
 // ----------------------------------------------------------------------
 
 const account = {
-  displayName: 'Khoa Dang',
-  username: 'khoanguyendanganh',
+  displayName: 'Duc An',
+  username: 'anduckhmt146',
   photoURL:
     'https://raw.githubusercontent.com/anduc146khmt/resource/master/public/assets/avatar_default.jpg',
 };
